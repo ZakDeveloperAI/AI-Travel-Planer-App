2fd87c46dbcf0e621042096c2c434f1551f7aeb4
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-01-19 16:27:25 +0100

    Trip section Done,started search for new trip section

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,10 +1,38 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { Colors } from './../../constants/Colors'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import StartNewTripCard from '../../components/MyTrips/StartNewTripCard';
 
 export default function MyTrip() {
+
+  const [userTrips, setUserTrips] = useState([]);
+
   return (
-    <View>
-      <Text>My Trip 1:27</Text>
+    <View style={{
+      padding:25,
+      paddingTop:20,
+      backgroundColor:Colors.WHITE,
+      height:'100%'
+    }}>
+      <View
+        style={{
+          display:'flex',
+          flexDirection:'row',
+          alignContent:'center',
+          justifyContent:'space-between'
+        }}
+      >
+          <Text style={{
+            fontFamily:'outfit-bold',
+            fontSize:35
+          }}>My Trips </Text>
+          <Ionicons name="add-circle" size={50} color="black" />
+        </View>
+        {userTrips?.length==0?
+          <StartNewTripCard/>:
+          null
+        }
     </View>
   )
 }
